423258f193b010f4c13fba35b2d9de5c3b4e758f
Lakshmi Muraleedharan <laxmimuraleedharan0@gmail.com>
2025-11-20 02:37:05 +0000

    updating model folder

--- models/mobilenet_model.py
+++ models/mobilenet_model.py
@@ -1,15 +1,18 @@
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.applications.MobileNetV2 import decode_prediction
 import numpy as np
-class MobileNetClassifier:
-    def _init_(self):
-        print("Loding of the model....")
+
+class MobileNetClassfier:
+    def __init__(self):
+        print("Loding of the model.....")
         self.model = MobileNetV2(weight = 'imagenet')
-        print('Model Ready')
+        print("Model Ready!")
+
     def predict(self, frames):
-        if len(frames.shape) ==3:
-            frames = np.expand_dims(frames,axis=0)
-        preds = self.model.predict(frames,verbose =1)
+        if len(frames.shape) == 3:
+            frames = np.expand_dims(frames, axis=0)
+        
+        preds = self.model.predict(frames, verbose = 1)
 
         results = []
         for pred in preds:
